0b799372fb66909cd3d9bfff4c96c12b687204b5
torch2424 <torch2424@Gojira-Senpaii.(none)>
2015-02-05 14:04:27 -0800

    Calcuscript with basic html and javascript

---
+++ js/calcuscript.js
@@ -0,0 +1,18 @@
+<SCRIPT LANGUAGE="JavaScript">
+<!-- hide script from old browsers
+//Our arraylists for our numbers and operators
+var operators = [];
+var numbers = [];
+
+//Function to compute answer and display it in text
+function compute()
+{
+	//Get the input from the text box
+	var input = document.getElementById('inputBox').value;
+	
+	//Output answer
+	document.getElementById('answer').value = "Sonic The Hedgehog";
+	
+}
+// end hiding script from old browsers -->
+</SCRIPT>
