24a83826cd40e70a20e17b5747559d8a08ae34e7
torch2424 <torch2424@hotmail.com>
2015-02-15 17:38:01 -0800

    Working parantheses support, some improvements of performMath() Function

--- src/browser_action/calcuscript.js
+++ src/browser_action/calcuscript.js
@@ -1,10 +1,21 @@
 //Our arraylists for our numbers and operators
 var operators = [];
+var priority = [];
+var operatorOrder = [];
 var numbers = [];
 var answer;
+
+//Regexes for fetching our input
+var operatorsRegex = /[-*x+/^%]/g;
+var numbersRegex = /[.0-9-*x+/^%]/g;
+var priorityRegex = /[-*x+/^%()]/g;
+
+
 //No longer doing order of operations
 //var order = ["^", "*", "/", "+", "-"];
 
+//JQuery to execute funtion once the html is loaded, so we don't get null
+//HTML document elements
 jQuery(document).ready(function($) {
 	var inputBox = document.getElementById("inputBox");
     inputBox.addEventListener("keyup",compute);
@@ -21,92 +32,86 @@ function compute()
 	//parse the string
 	parseInput(input);
 	
-	//performing math
-	//Need to makesure opertators is not null or zero
-	if(operators != null && operators.length > 0)
-	{
-		//perform math
-		//make order of operations array
-		//loop for order of operations
-		console.log("START!!!!!!!!");
-			//loop for doing math if those operators exist
-			for(ii = 0; ii < operators.length; ii++)
-			{
-				//If we are currently doing the operator in order of operations,
-				//and they are more numbers than operators or an equal amount (as it should be)
-				if(numbers.length >= operators.length && numbers[ii + 1] != null)
-				{
-					console.log("entered the matrix");
-					var tempAnswer;
-					//Exponent
-					if(operators[ii] == "^")
-					{
-						//The orignial number we are powering
-						var base = numbers[ii];
-						tempAnswer = base;
-						//multiple number by itself that many times
-						//Start one since we dont start exponents at zero
-						for(var j = 1; j < numbers[ii + 1]; j++)
-						{
-							tempAnswer = tempAnswer * base;
-						}
-					}
-					//Multiplication
-					else if(operators[ii] == "*" || operators[ii] == "x")
-					{
-						console.log(numbers[ii] + "," + numbers[ii + 1]);
-						tempAnswer = numbers[ii] * numbers[ii + 1];
-					}
-					//Division
-					else if(operators[ii] == "/")
-					{
-						tempAnswer = numbers[ii] / numbers[ii + 1];
-					}
-					//Addition
-					else if(operators[ii] == "+")
-					{
-						tempAnswer = numbers[ii] + numbers[ii + 1];
-						console.log(numbers[ii] + "," + numbers[ii + 1]);
-					}
-					//Subtraction
-					else if(operators[ii] == "-")
-					{
-						tempAnswer = numbers[ii] - numbers[ii + 1];
-					}
-					
-					console.log("here is temp answer now: " + tempAnswer);
-					//Lastly replace the old numbers and remove the operators
-					//Using splice method
-					numbers.splice(ii, 2, tempAnswer);
-					operators.splice(ii, 1);
-					
-					//since there may be another of this operator, check for this operator again, by subtracting 1 from ii
-					ii--;
-				}
-				//Log the process
-				console.log("iteration " + ii + " leaves the numbers: " + numbers + "\nand operators: " + operators);
-			}
-		}
-	//The last remaining number is the answer
-	if(numbers != null && numbers.length > 0)
-	{
-		console.log("final answer: " + numbers[0]);
-		answer = numbers[0];
-	}
+	//Use the parse string to do calculation
+	performMath();
+	
 	//Output answer, working
-	document.getElementById('answer').innerHTML = answer;
+	//If the input is empty, else show the answer
+	if(inputBox.value == null || inputBox.value == "")
+	{
+		document.getElementById('answer').innerHTML = "";
+	}
+	else
+	{
+		document.getElementById('answer').innerHTML = answer;
+	}
+	
+	//Now reset our variables
+	//Our arraylists for our numbers and operators
+	operators = [];
+	priority = [];
+	operatorOrder = [];
+	numbers = [];
+	answer = 0;
 	
 }
 
 //Function to parse our input into operators and numbers
 function parseInput(math)
 {
-	var operatorsRegex = /[-*x+/^]/g;
 	//parse the numbers and operators
-	//Using Character quotes for ^ since its special
 	operators = math.match(operatorsRegex);
+	
+	//Create our operator priority array
+	priority = math.match(priorityRegex);
+	
+	//Doing a loop to compare how many parntheses left brackets and right brackets
+	//surround an operator,
+	//and then create an array (operator order) to represent which operator should be performed
+	//Left and right paran is to keep count of how many parantheses we see as we go along
+	//should be paralel to operator index since we only have added parantheses in this string
+	//left brackets - right brackets = priority, since we are reading from left to right
+	//left starts priority, and right would be closing it
+	if(priority != null)
+	{
+		var leftParan = 0;
+		var rightParan = 0;
+		for(i = 0; i < priority.length; i++)
+		{
+			//If it is not a parantheses, then it is an operator
+			if(priority[i] != "(" && priority[i] != ")")
+			{
+					//Now that we know how many left and right parantheses surround it,
+					//we can give it a priority, but it can never be greater than the left
+					//parentheses since that starts our experssion
+					var finalPri = 0;
+					if(leftParan - rightParan < 0)
+					{
+						finalPri = 0;
+					}
+					else
+					{
+						finalPri = leftParan - rightParan;
+					}
+					
+					//Add to our array
+					operatorOrder.push(finalPri);
+			}
+			//Keep count of how many left parantheses we find as 
+			//we go along to save processing power
+			else if(priority[i] == "(")
+			{
+				leftParan++;
+			}
+			else if(priority[i] == ")")
+			{
+				rightParan++;
+			}
+		}
+	}
+	
 	//Putting all digits, persiods and opertators into an array
-	tempEquation = math.match(/[.0-9-*x+/^]/g);
+	tempEquation = math.match(numbersRegex);
 	//Now we are getting all the floats from the array, by adding all digits and periods to 
 	//a string, and then parsing that string for floats
 	if(tempEquation != null)
@@ -131,3 +136,93 @@ function parseInput(math)
 		}
 	}
 }
+
+function performMath()
+{
+		//performing math
+		//Need to makesure opertators is not null or zero
+		if(operators != null && operators.length > 0)
+		{
+			//perform math
+			//make order of operations array
+			//loop for order of operations
+			console.log("START!!!!!!!!");
+			console.log(operators);
+			console.log(operatorOrder);
+				//loop for doing math if those operators exist
+				while(operators.length > 0 && numbers.length >= operators.length)
+				{
+					console.log("priority array: " + operatorOrder);
+					//Find the highest priority operator
+					var index = 0;
+					for(i = 0; i < operatorOrder.length; i++)
+					{
+						if(operatorOrder[i] > operatorOrder[index])
+						{
+							index = i;
+						}
+					}
+					
+					//If we are currently doing the operator in order of operations,
+					//and they are more numbers than operators or an equal amount (as it should be)
+					if(numbers[index + 1] != null)
+					{
+						console.log("entered the matrix");
+						var tempAnswer;
+						//Exponent
+						if(operators[index] == "^")
+						{
+							tempAnswer = Math.pow(numbers[index], numbers[index + 1]);
+						}
+						//Multiplication
+						else if(operators[index] == "*" || operators[index] == "x")
+						{
+							console.log(numbers[index] + "," + numbers[index + 1]);
+							tempAnswer = numbers[index] * numbers[index + 1];
+						}
+						//Division
+						else if(operators[index] == "/")
+						{
+							tempAnswer = numbers[index] / numbers[index + 1];
+						}
+						//Modulo
+						else if(operators[index] == "%")
+						{
+							tempAnswer = numbers[index] % numbers[index + 1];
+						}
+						//Addition
+						else if(operators[index] == "+")
+						{
+							tempAnswer = numbers[index] + numbers[index + 1];
+							console.log(numbers[index] + "," + numbers[index + 1]);
+						}
+						//Subtraction
+						else if(operators[index] == "-")
+						{
+							tempAnswer = numbers[index] - numbers[index + 1];
+						}
+						
+						console.log("here is temp answer now: " + tempAnswer);
+						//Lastly replace the old numbers and remove the operators
+						//Using splice method
+						numbers.splice(index, 2, tempAnswer);
+						operators.splice(index, 1);
+						//also need to splice our priority
+						operatorOrder.splice(index, 1);
+					}
+					//failsafe to exit the loop when we have even number of operators 
+					//and stuff after evaluating an expressions
+					else if(numbers.length == operators.length)
+					{
+						operators.pop();
+					}
+				}
+			}
+		//The last remaining number is the answer
+		if(numbers != null && numbers.length > 0)
+		{
+			console.log("final answer: " + numbers[0]);
+			answer = numbers[0];
+		}
+		
+	}
