62317958c0831ac1bd3d93e30539bd309cbefb2c
Aaron <torch2424@hotmail.com>
2015-12-20 00:31:43 -0800

    Converted To ES6
    Converted the entire project ot ES6, and into a newly scaffolded in.
    
    Now To simply add css and localstorage!

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,5 @@
+'use strict';
+
 //Our arraylists for our numbers and operators
 var operators = [];
 var priority = [];
@@ -5,22 +7,21 @@ var operatorOrder = [];
 var numbers = [];
 var answer;
 
+//Our Main Input
+var inputBox;
+
 //Adding "r" to mean root!
 //Regexes for fetching our input
 var operatorsRegex = /[-*xX+/^%rR]/g;
 var numbersRegex = /[.0-9-*xX+/^%rR]/g;
 var priorityRegex = /[-*xX+/^%()rR]/g;
 
-
-//No longer doing order of operations
-//var order = ["^", "*", "/", "+", "-"];
-
-//JQuery to execute funtion once the html is loaded, so we don't get null
-//HTML document elements
-jQuery(document).ready(function($) {
-	var inputBox = document.getElementById("inputBox");
+// Initialize on page load
+document.addEventListener('DOMContentLoaded', function() {
+    inputBox = document.getElementById("inputBox");
     inputBox.addEventListener("keyup",compute);
-    
+    inputBox.addEventListener("keydown",compute);
+
     var clearButton = document.getElementById("clearButton");
     clearButton.addEventListener("click",clear);
 });
@@ -30,15 +31,16 @@ function compute()
 {
 	operators = [];
 	numbers = [];
+
 	//Get the input from the text box, working
-	var input = document.getElementById('inputBox').value;
-	
+	let input = document.getElementById('inputBox').value;
+
 	//parse the string
 	parseInput(input);
-	
+
 	//Use the parse string to do calculation
 	performMath();
-	
+
 	//Output answer, working
 	//If the input is empty, else show the answer
 	if(inputBox.value == null || inputBox.value == "")
@@ -49,7 +51,7 @@ function compute()
 	{
 		document.getElementById('answer').innerHTML = answer;
 	}
-	
+
 	//Now reset our variables
 	//Our arraylists for our numbers and operators
 	operators = [];
@@ -57,7 +59,7 @@ function compute()
 	operatorOrder = [];
 	numbers = [];
 	answer = 0;
-	
+
 }
 
 //Function to parse our input into operators and numbers
@@ -65,10 +67,10 @@ function parseInput(math)
 {
 	//parse the numbers and operators
 	operators = math.match(operatorsRegex);
-	
+
 	//Create our operator priority array
 	priority = math.match(priorityRegex);
-	
+
 	//Doing a loop to compare how many parntheses left brackets and right brackets
 	//surround an operator,
 	//and then create an array (operator order) to represent which operator should be performed
@@ -78,9 +80,9 @@ function parseInput(math)
 	//left starts priority, and right would be closing it
 	if(priority != null)
 	{
-		var leftParan = 0;
-		var rightParan = 0;
-		for(i = 0; i < priority.length; i++)
+		let leftParan = 0;
+		let rightParan = 0;
+		for(let i = 0; i < priority.length; i++)
 		{
 			//If it is not a parantheses, then it is an operator
 			if(priority[i] != "(" && priority[i] != ")")
@@ -88,7 +90,7 @@ function parseInput(math)
 					//Now that we know how many left and right parantheses surround it,
 					//we can give it a priority, but it can never be greater than the left
 					//parentheses since that starts our experssion
-					var finalPri = 0;
+					let finalPri = 0;
 					if(leftParan - rightParan < 0)
 					{
 						finalPri = 0;
@@ -97,11 +99,11 @@ function parseInput(math)
 					{
 						finalPri = leftParan - rightParan;
 					}
-					
+
 					//Add to our array
 					operatorOrder.push(finalPri);
 			}
-			//Keep count of how many left parantheses we find as 
+			//Keep count of how many left parantheses we find as
 			//we go along to save processing power
 			else if(priority[i] == "(")
 			{
@@ -113,15 +115,15 @@ function parseInput(math)
 			}
 		}
 	}
-	
+
 	//Putting all digits, persiods and opertators into an array
-	tempEquation = math.match(numbersRegex);
-	//Now we are getting all the floats from the array, by adding all digits and periods to 
+	let tempEquation = math.match(numbersRegex);
+	//Now we are getting all the floats from the array, by adding all digits and periods to
 	//a string, and then parsing that string for floats
 	if(tempEquation != null)
 	{
-		var tempFloat = "";
-		for(i = 0; i < tempEquation.length; i++)
+		let tempFloat = "";
+		for(let i = 0; i < tempEquation.length; i++)
 		{
 			if(operatorsRegex.test(tempEquation[i]) == false)
 			{
@@ -131,7 +133,7 @@ function parseInput(math)
 				{
 					numbers.push(parseFloat(tempFloat));
 				}
-			} 
+			}
 			else if(tempFloat != null && tempFloat != "")
 			{
 				numbers.push(parseFloat(tempFloat));
@@ -150,32 +152,24 @@ function performMath()
 			//perform math
 			//make order of operations array
 			//loop for order of operations
-			console.log("START!!!!!!!!");
 				//loop for doing math if those operators exist
 				while(operators.length > 0 && numbers.length >= operators.length)
 				{
-					console.log("Operator order array: " + operatorOrder);
-					console.log("Operator array: " + operators);
-					console.log("Numbersarray: " + numbers);
 					//Find the highest priority operator
-					var index = 0;
-					for(i = 0; i < operatorOrder.length; i++)
+					let index = 0;
+					for(let i = 0; i < operatorOrder.length; i++)
 					{
 						if(operatorOrder[i] > operatorOrder[index])
 						{
 							index = i;
 						}
 					}
-					
-					console.log("Index: " + index);
-					console.log("numbers[index + 1]: " + numbers[index + 1]);
-					
+
 					//If we are currently doing the operator in order of operations,
 					//and they are more numbers than operators or an equal amount (as it should be)
 					if(numbers[index + 1] != null)
 					{
-						console.log("entered the matrix");
-						var tempAnswer;
+						let tempAnswer;
 						//Exponent
 						if(operators[index] == "^")
 						{
@@ -191,7 +185,6 @@ function performMath()
 						else if(operators[index] == "*" || operators[index] == "x"
 						|| operators[index] == "X")
 						{
-							console.log(numbers[index] + "," + numbers[index + 1]);
 							tempAnswer = numbers[index] * numbers[index + 1];
 						}
 						//Division
@@ -208,15 +201,13 @@ function performMath()
 						else if(operators[index] == "+")
 						{
 							tempAnswer = numbers[index] + numbers[index + 1];
-							console.log(numbers[index] + "," + numbers[index + 1]);
 						}
 						//Subtraction
 						else if(operators[index] == "-")
 						{
 							tempAnswer = numbers[index] - numbers[index + 1];
 						}
-						
-						console.log("here is temp answer now: " + tempAnswer);
+
 						//Lastly replace the old numbers and remove the operators
 						//Using splice method
 						numbers.splice(index, 2, tempAnswer);
@@ -238,37 +229,21 @@ function performMath()
 				}
 			}
 		//The last remaining number is the answer
-		if(numbers != null && numbers.length > 0)
-		{
-			console.log("final answer: " + numbers[0]);
-			answer = numbers[0];
-		}
-		
+		if(numbers != null && numbers.length > 0) answer = numbers[0];
+
 	}
-	
+
 //function to clear the screen through the clear button
 function clear()
 {
 	//Get our variables
 	var inputValue = document.getElementById("inputBox");
 	var answer = document.getElementById('answer');
-	
-	
-	
-	//Fade our our variables
-	$(inputValue).add(answer).animate
-	({opacity: 0.0}, 500,
-	function()
-	{
-		//Input Box and answer
-		document.getElementById('inputBox').value = "";
-		document.getElementById('answer').innerHTML = "";
-	});
-	
-	//Fade back in
-	$(inputValue).add(answer).animate
-	({opacity: 1.0}, 500);
-	
+
+	//Input Box and answer
+	document.getElementById('inputBox').value = "";
+	document.getElementById('answer').innerHTML = "";
+
 	//to prevent going back to the top of the page
 	event.preventDefault();
 }
